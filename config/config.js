415f4da9fb10bfbfc549dbf17600597e1122e9d3
Kelly <xiaohandang@gmail.com>
2018-08-08 17:02:54 -0700

    Rewrote project. Does not yet have create wallet features

---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  etherscanApiKey: '45Q8D9AGGM8RI33WIZTM6HV2AAC919PKQ4',
+  gasPrice: '10',
+  gasLimit: 70000,
+}
---
+++ server.js
@@ -0,0 +1,137 @@
+const config = require('./config/config.js');
+var express = require('express');
+var app = express();
+const formidable = require('formidable');
+const fs = require('fs');
+const parse = require('csv-parse/lib/sync');
+const Web3 = require('web3');
+const request = require('request');
+const delay = require('delay');
+const Tx = require('ethereumjs-tx');
+var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/vCfQu4uCspVZEATQTcmJ'));
+
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+	path: './outputs/transactions.csv',
+	header: [{
+			id: 'Name',
+			title: 'Name'
+		},
+		{
+			id: 'Address',
+			title: 'Address'
+		},
+		{
+			id: 'Amount',
+			title: 'Amount'
+		},
+		{
+			id: 'TxHash',
+			title: 'TxHash'
+		},
+
+	]
+});
+
+var transactionRecords = [];
+// var arrayCounter = 0;
+
+app.post('/send', (req, res) => {
+	var form = new formidable.IncomingForm();
+	form.parse(req, (err, fields, files) => {
+
+		if (!fields.fromAddress || !fields.fromPrivateKey || files.destinations.size === 0) {
+			console.log("Invalid user input");
+		} else {
+			var myAddress = fields.fromAddress;
+			var myPrivateKey = new Buffer(fields.fromPrivateKey, 'hex');
+			var contractAddress = fields.contractAddress;
+
+			let etherscanURL = 'https://api-ropsten.etherscan.io/api?module=contract&action=getabi&address=' + contractAddress + '&apikey=' + config.etherscanApiKey;
+
+			input = parse(fs.readFileSync(files.destinations.path, 'utf-8'), {
+				columns: true
+			});
+			request(etherscanURL, (error, response, data) => {
+				var abiArray = JSON.parse(JSON.parse(data).result);
+				var contract = new web3.eth.Contract(abiArray, contractAddress);
+
+				web3.eth.getTransactionCount(myAddress).then((transactionCount) => {
+					sendTokens(myAddress, myPrivateKey, input, transactionCount, contract, contractAddress);
+					writeToCSV(input);
+				});
+			});
+		}
+		setTimeout(() => {
+			res.redirect('https://ropsten.etherscan.io/address/' + myAddress);
+		}, 5000);
+	});
+});
+
+// app.use(bodyParser.urlencoded({
+//   extended:true
+// }));
+
+var port = 8080;
+app.listen(port, () => console.log("Listening on port: " + port));
+
+
+function sendTokens(myAddress, myPrivateKey, input, transactionCount, contract, contractAddress) {
+
+	for (let i = 0; i < input.length; i++) {
+		var element = input[i];
+
+		var toAddress = element.Address;
+		var amount = element.Amount;
+		var name = element.Name;
+
+		var rawTransaction = buildRawTransaction(transactionCount, toAddress, amount, contract, contractAddress);
+		var tx = new Tx(rawTransaction);
+
+		tx.sign(myPrivateKey);
+		var serializedTx = '0x' + tx.serialize().toString('hex');
+
+    sendToken(serializedTx, toAddress, amount, name);
+
+		transactionCount++;
+	}
+}
+
+function sendToken(serializedTx, toAddress, amount, name) {
+  web3.eth.sendSignedTransaction(serializedTx)
+    .on('transactionHash', (hash) => {
+      let transactionRecord = {
+        Name: name,
+        Address: toAddress,
+        Amount: amount,
+        TxHash: hash
+      }
+
+      transactionRecords.push(transactionRecord);
+      console.log(hash);
+      // arrayCounter++;
+    })
+}
+
+function buildRawTransaction(nonce, toAddress, amount, contract, contractAddress) {
+	var data = contract.methods.transfer(toAddress, amount).encodeABI();
+	return {
+		"nonce": nonce,
+		"gasPrice": Web3.utils.toHex(web3.utils.toWei(config.gasPrice, "shannon")),
+		"gasLimit": web3.utils.toHex(config.gasLimit),
+		"to": contractAddress,
+		"value": web3.utils.toHex(0),
+		"data": data,
+		"chainId": 0x03,
+	}
+}
+
+async function writeToCSV(input) {
+	while (transactionRecords.length !== input.length) {
+		await delay(2000);
+	}
+	csvWriter.writeRecords(transactionRecords)
+		.then(() => {
+			console.log("...Done");
+		});
+}
